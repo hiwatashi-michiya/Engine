79f06a78ae6a3e8e8308c35079121cda1124e407
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2023-11-14 12:58:49 +0900

    Particle3Dのオブジェクトの一部をCpmPtr化

--- Engine/3d/Particle3D.h
+++ Engine/3d/Particle3D.h
@@ -60,14 +60,14 @@ private:
 	//コマンドリスト
 	static ID3D12GraphicsCommandList* commandList_;
 	//ルートシグネチャ
-	static ID3D12RootSignature* rootSignature_;
+	static Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_;
 	//PSO
-	static ID3D12PipelineState* particlePipelineStates_[BlendMode::kCountBlend];
+	static Microsoft::WRL::ComPtr<ID3D12PipelineState> particlePipelineStates_[BlendMode::kCountBlend];
 
 	/*static ID3D12PipelineState* pipelineState_;*/
 
-	static IDxcBlob* vs3dParticleBlob_;
-	static IDxcBlob* ps3dParticleBlob_;
+	static Microsoft::WRL::ComPtr<IDxcBlob> vs3dParticleBlob_;
+	static Microsoft::WRL::ComPtr<IDxcBlob> ps3dParticleBlob_;
 
 	//モデル識別用変数(ImGuiで使用)
 	static int modelNumber_;
