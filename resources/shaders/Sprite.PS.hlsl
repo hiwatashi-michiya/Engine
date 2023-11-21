14965e68dc536f084016a92ea49da537ae169fcc
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2023-11-21 11:17:08 +0900

    テクスチャの透過部分を棄却するように変更

--- resources/shaders/Sprite.PS.hlsl
+++ resources/shaders/Sprite.PS.hlsl
@@ -16,8 +16,16 @@ SamplerState gSampler : register(s0);
 PixelShaderOutput main(VertexShaderOutput input) {
 
 	PixelShaderOutput output;
+	
 	/*float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);*/
 	float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+	
+	//textureのα値が0.5以下のときにPixelを棄却
+    if (textureColor.a <= 0.5)
+    {
+        discard;
+    }
+	
 	output.color = gMaterial.color * textureColor;
 
 	return output;
