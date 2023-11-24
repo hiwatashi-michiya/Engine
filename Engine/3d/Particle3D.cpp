dcd55a29707ba1c650d742256eedab0c98f6d9d7
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2023-11-24 14:11:13 +0900

    パーティクル追加

---
+++ Engine/3d/Particle3D.cpp
@@ -0,0 +1,580 @@
+#include "Particle3D.h"
+#include <cassert>
+#include "Engine/Convert.h"
+#include "Engine/manager/ShaderManager.h"
+#include <fstream>
+#include <sstream>
+#include "Engine/manager/ImGuiManager.h"
+#include "Model.h"
+
+#pragma comment(lib, "dxcompiler.lib")
+
+ID3D12Device* Particle3D::device_ = nullptr;
+ID3D12GraphicsCommandList* Particle3D::commandList_ = nullptr;
+ID3D12RootSignature* Particle3D::rootSignature_ = nullptr;
+ID3D12PipelineState* Particle3D::particlePipelineStates_[Particle3D::BlendMode::kCountBlend] = { nullptr };
+//ID3D12PipelineState* Particle3D::pipelineState_ = nullptr;
+IDxcBlob* Particle3D::vs3dParticleBlob_ = nullptr;
+IDxcBlob* Particle3D::ps3dParticleBlob_ = nullptr;
+WorldTransform Particle3D::worldTransformCamera_{};
+Matrix4x4 Particle3D::matProjection_ = MakeIdentity4x4();
+int Particle3D::modelNumber_ = 0;
+Particle3D::BlendMode Particle3D::currentBlendMode_ = Particle3D::BlendMode::kNormal;
+const char* Particle3D::BlendTexts[Particle3D::BlendMode::kCountBlend] = { "Normal", "Add", "Subtract", "Multiply", "Screen" };
+
+//mtlファイル読み込み関数
+Particle3D::MaterialData LoadMaterialTemplateFile(const std::string& filename, const std::string& mtlFilename) {
+
+	//1.中で必要となる変数の宣言
+	Particle3D::MaterialData materialData; //構築するMaterialData
+	std::string line; //ファイルから読んだ1行を格納するもの
+
+	//2.ファイルを開く
+	std::ifstream file(modelDirectoryPath + "/" + filename + "/" + mtlFilename); //ファイルを開く
+	assert(file.is_open()); //開けなかったら止める
+
+	//3.実際にファイルを読み、MaterialDataを構築していく
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		//identifierに応じた処理
+		if (identifier == "map_Kd") {
+			std::string textureFilename;
+			s >> textureFilename;
+			//連結してファイルパスにする
+			materialData.textureFilePath = modelDirectoryPath + "/" + filename + "/" + textureFilename;
+		}
+
+	}
+
+	//4.MaterialDataを返す
+	return materialData;
+
+}
+
+//Objファイル読み込み関数
+Particle3D::ModelData LoadObjFile(const std::string& filename) {
+
+	//1.必要となる変数の宣言
+	Particle3D::ModelData modelData; //構築するModelData
+	std::vector<Vector4> positions; //位置
+	std::vector<Vector3> normals; //法線
+	std::vector<Vector2> texcoords; //テクスチャ座標
+	std::string line; //ファイルから読んだ1行を格納するもの
+
+	//2.ファイルを開く
+	std::ifstream file(modelDirectoryPath + "/" + filename + "/" + filename + ".obj"); //ファイルを開く
+	assert(file.is_open()); //開けなかったら止める
+
+	//3.実際にファイルを読み、ModelDataを構築していく
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier; //先頭の識別子を読む
+
+		//identifierに応じた処理
+		if (identifier == "v") {
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			//座標系の変換のため、zを反転
+			position.z *= -1.0f;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoord.y = 1.0f - texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			//座標系の変換のため、zを反転
+			normal.z *= -1.0f;
+			normals.push_back(normal);
+		}
+		else if (identifier == "f") {
+			Particle3D::VertexData triangle[3];
+			//面は三角形限定。その他は未対応
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+				//頂点の要素へのIndexは「位置/UV/法線」で格納されているので、分解してIndexを取得する
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					std::getline(v, index, '/'); //区切りでインデックスを読んでいく
+
+					if (index == "") {
+						//情報が無い場合、1を代入
+						elementIndices[element] = 1;
+					}
+					else {
+						elementIndices[element] = std::stoi(index);
+					}
+
+				}
+
+				//要素へのIndexから、実際の要素の値を取得して、頂点を構築する
+				Vector4 position = positions[elementIndices[0] - 1];
+
+				Vector2 texcoord;
+				if (!texcoords.empty()) {
+					texcoord = texcoords[elementIndices[1] - 1];
+				}
+				else {
+					texcoord = { 0.0f,0.0f };
+				}
+				Vector3 normal = normals[elementIndices[2] - 1];
+				/*VertexData vertex = { position, texcoord, normal };
+				modelData.vertices.push_back(vertex);*/
+				triangle[faceVertex] = { position, texcoord, normal };
+
+			}
+			//頂点を逆順で登録することで、回り順を逆にする
+			modelData.vertices.push_back(triangle[2]);
+			modelData.vertices.push_back(triangle[1]);
+			modelData.vertices.push_back(triangle[0]);
+
+		}
+		else if (identifier == "mtllib") {
+			//materialTemplateLibraryファイルの名前を取得する
+			std::string materialFilename;
+			s >> materialFilename;
+			//基本的にobjファイルと同一階層にmtlは存在させるので、ディレクトリ名とファイル名を渡す
+			modelData.material = LoadMaterialTemplateFile(filename, materialFilename);
+
+		}
+
+	}
+
+	//4.ModelDataを返す
+	return modelData;
+
+}
+
+void Particle3D::StaticInitialize(ID3D12Device* device) {
+
+	assert(device);
+
+	HRESULT hr;
+
+	device_ = device;
+
+	//dxcCompilerを初期化
+	Microsoft::WRL::ComPtr<IDxcUtils> dxcUtils = nullptr;
+	Microsoft::WRL::ComPtr<IDxcCompiler3> dxcCompiler = nullptr;
+	hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
+	assert(SUCCEEDED(hr));
+	hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+	assert(SUCCEEDED(hr));
+
+	//includeに対応するための設定
+	Microsoft::WRL::ComPtr<IDxcIncludeHandler> includeHandler = nullptr;
+	hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+	assert(SUCCEEDED(hr));
+
+	//Shaderをコンパイルする
+	vs3dParticleBlob_ = CompileShader(L"./resources/shaders/Particle3d.VS.hlsl",
+		L"vs_6_0", dxcUtils, dxcCompiler, includeHandler);
+	assert(vs3dParticleBlob_ != nullptr);
+
+	ps3dParticleBlob_ = CompileShader(L"./resources/shaders/Particle3d.PS.hlsl",
+		L"ps_6_0", dxcUtils, dxcCompiler, includeHandler);
+	assert(ps3dParticleBlob_ != nullptr);
+
+	//頂点レイアウト
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[3] = {};
+	inputElementDescs[0].SemanticName = "POSITION";
+	inputElementDescs[0].SemanticIndex = 0;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	inputElementDescs[2].SemanticName = "NORMAL";
+	inputElementDescs[2].SemanticIndex = 0;
+	inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+	//RootSignature作成
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+	descriptionRootSignature.Flags =
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	//D3D12_ROOT_PARAMETER rootParameters[1];
+	//rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	//rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	//rootParameters[0].Descriptor.ShaderRegister = 0;
+	//descriptionRootSignature.pParameters = rootParameters; //ルートパラメータ配列へのポインタ
+	//descriptionRootSignature.NumParameters = 1; //ルートパラメータの長さ
+
+	D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
+	descriptorRange[0].BaseShaderRegister = 0;
+	descriptorRange[0].NumDescriptors = 1;
+	descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	descriptorRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+	D3D12_DESCRIPTOR_RANGE descriptorRangeForInstancing[1] = {};
+	descriptorRangeForInstancing[0].BaseShaderRegister = 0;
+	descriptorRangeForInstancing[0].NumDescriptors = 1;
+	descriptorRangeForInstancing[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	descriptorRangeForInstancing[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+	//ルートパラメータ作成
+	D3D12_ROOT_PARAMETER rootParameters[3]{};
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	rootParameters[0].Descriptor.ShaderRegister = 0;
+
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; //DescriptorTableを使う
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX; //VertexShaderで使う
+	rootParameters[1].DescriptorTable.pDescriptorRanges = descriptorRangeForInstancing; //Tableの中身の配列を指定
+	rootParameters[1].DescriptorTable.NumDescriptorRanges = _countof(descriptorRangeForInstancing); //Tableで利用する数
+
+	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; //DescriptorTableを使う
+	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; //PixelShaderで使う
+	rootParameters[2].DescriptorTable.pDescriptorRanges = descriptorRange; //Tableの中身の配列を指定
+	rootParameters[2].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange); //Tableで利用する数
+
+	descriptionRootSignature.pParameters = rootParameters; //ルートパラメータ配列へのポインタ
+	descriptionRootSignature.NumParameters = _countof(rootParameters); //ルートパラメータの長さ
+
+	//Samplerの設定
+	D3D12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
+	staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR; //バイリニアフィルタ
+	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP; //0～1の範囲外をリピート
+	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER; //比較しない
+	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX; //ありったけのMipmapを使う
+	staticSamplers[0].ShaderRegister = 0; //レジスタ番号0を使う
+	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; //PixelShaderで使う
+	descriptionRootSignature.pStaticSamplers = staticSamplers;
+	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
+
+	//シリアライズしてバイナリにする
+	Microsoft::WRL::ComPtr<ID3DBlob> signatureBlob = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob = nullptr;
+	hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob);
+	if (FAILED(hr)) {
+		Log(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		assert(false);
+	}
+	//バイナリを元に生成
+	hr = device_->CreateRootSignature(0, signatureBlob->GetBufferPointer(),
+		signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature_));
+	assert(SUCCEEDED(hr));
+
+	//Blendstateの設定
+	D3D12_BLEND_DESC blendDesc{};
+	//全ての色要素を書き込む
+	blendDesc.RenderTarget[0].RenderTargetWriteMask =
+		D3D12_COLOR_WRITE_ENABLE_ALL;
+	blendDesc.RenderTarget[0].BlendEnable = TRUE;
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+
+	//RasterizerStateの設定
+	D3D12_RASTERIZER_DESC rasterizerDesc{};
+	//裏面(時計回り)を表示しない
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	//三角形の中を塗りつぶす
+	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
+	graphicsPipelineStateDesc.pRootSignature = rootSignature_; //RootSignature
+	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc; //InputLayout
+	graphicsPipelineStateDesc.VS = { vs3dParticleBlob_->GetBufferPointer(),
+	vs3dParticleBlob_->GetBufferSize() }; //VertexShader
+	graphicsPipelineStateDesc.PS = { ps3dParticleBlob_->GetBufferPointer(),
+	ps3dParticleBlob_->GetBufferSize() }; //PixelShader
+	graphicsPipelineStateDesc.RasterizerState = rasterizerDesc; //RasterizerState
+	//書き込むRTVの情報
+	graphicsPipelineStateDesc.NumRenderTargets = 1;
+	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	//利用するトポロジ(形状)のタイプ、三角形
+	graphicsPipelineStateDesc.PrimitiveTopologyType =
+		D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	//どのように画面に色を打ち込むかの設定(気にしなくて良い)
+	graphicsPipelineStateDesc.SampleDesc.Count = 1;
+	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+	//DepthStencilStateの設定
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc{};
+	//Depthの機能を有効化する
+	depthStencilDesc.DepthEnable = true;
+	//書き込み
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+	//近ければ描画
+	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
+	//DepthStencilの設定
+	graphicsPipelineStateDesc.DepthStencilState = depthStencilDesc;
+	graphicsPipelineStateDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;
+
+	//通常
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+	graphicsPipelineStateDesc.BlendState = blendDesc; //BlendState
+	//グラフィックスパイプラインを実際に生成
+	hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
+		IID_PPV_ARGS(&particlePipelineStates_[BlendMode::kNormal]));
+	assert(SUCCEEDED(hr));
+
+	//加算
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+	graphicsPipelineStateDesc.BlendState = blendDesc; //BlendState
+	//グラフィックスパイプラインを実際に生成
+	hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
+		IID_PPV_ARGS(&particlePipelineStates_[BlendMode::kAdd]));
+	assert(SUCCEEDED(hr));
+
+	//減算
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_SUBTRACT;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+	graphicsPipelineStateDesc.BlendState = blendDesc; //BlendState
+	//グラフィックスパイプラインを実際に生成
+	hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
+		IID_PPV_ARGS(&particlePipelineStates_[BlendMode::kSubtract]));
+	assert(SUCCEEDED(hr));
+
+	//乗算
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_ZERO;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_SRC_COLOR;
+	graphicsPipelineStateDesc.BlendState = blendDesc; //BlendState
+	//グラフィックスパイプラインを実際に生成
+	hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
+		IID_PPV_ARGS(&particlePipelineStates_[BlendMode::kMultiply]));
+	assert(SUCCEEDED(hr));
+
+	//スクリーン
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_INV_DEST_COLOR;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ONE;
+	graphicsPipelineStateDesc.BlendState = blendDesc; //BlendState
+	//グラフィックスパイプラインを実際に生成
+	hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
+		IID_PPV_ARGS(&particlePipelineStates_[BlendMode::kScreen]));
+	assert(SUCCEEDED(hr));
+
+	currentBlendMode_ = BlendMode::kNormal;
+
+}
+
+Particle3D* Particle3D::Create(const std::string& filename, uint32_t instanceCount) {
+
+	Particle3D* particle3d = new Particle3D();
+
+	particle3d->Initialize(filename, instanceCount);
+
+	return particle3d;
+
+}
+
+void Particle3D::Initialize(const std::string& filename, uint32_t instanceCount) {
+
+	assert(device_);
+
+	modelData_ = LoadObjFile(filename);
+
+	instanceCount_ = instanceCount;
+
+	//頂点バッファ
+	{
+
+		vertBuff_ = CreateBufferResource(device_, sizeof(VertexData) * modelData_.vertices.size());
+
+		//頂点バッファビュー設定
+		vbView_.BufferLocation = vertBuff_->GetGPUVirtualAddress();
+		vbView_.SizeInBytes = sizeof(VertexData) * modelData_.vertices.size();
+		vbView_.StrideInBytes = sizeof(VertexData);
+
+		//マッピングしてデータ転送
+		vertBuff_->Map(0, nullptr, reinterpret_cast<void**>(&vertMap_));
+
+		std::memcpy(vertMap_, modelData_.vertices.data(), sizeof(VertexData) * modelData_.vertices.size());
+
+		//アンマップ
+		vertBuff_->Unmap(0, nullptr);
+
+	}
+
+
+	//定数バッファ
+	{
+
+		constBuff_ = CreateBufferResource(device_, sizeof(Material));
+
+		//マッピングしてデータ転送
+		constBuff_->Map(0, nullptr, reinterpret_cast<void**>(&constMap_));
+
+		constMap_->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+		constMap_->enableLighting = true;
+		constMap_->uvTransform = MakeIdentity4x4();
+
+		//アンマップ
+		constBuff_->Unmap(0, nullptr);
+
+	}
+
+	//transformMatrix
+	{
+
+		matBuff_ = CreateBufferResource(device_, sizeof(TransformationMatrix) * instanceCount_);
+
+		matBuff_->Map(0, nullptr, reinterpret_cast<void**>(&matTransformMap_));
+
+		//インスタンシングの数だけループ
+		for (uint32_t i = 0; i < instanceCount_; i++) {
+			matTransformMap_[i].WVP = MakeIdentity4x4();
+			matTransformMap_[i].World = MakeIdentity4x4();
+		}
+
+		matBuff_->Unmap(0, nullptr);
+
+	}
+
+	//インスタンシングリソース設定
+	{
+
+		instancingResource_ = TextureManager::GetInstance()->SetInstancingResource(instanceCount_, matBuff_);
+
+	}
+
+	//テクスチャ設定
+	{
+
+		//テクスチャ情報が空でない場合に設定
+		if (modelData_.material.textureFilePath != "") {
+			texture_ = TextureManager::GetInstance()->Load(modelData_.material.textureFilePath);
+		}
+		//テクスチャ情報が空の場合、既定の画像に設定
+		else {
+			texture_ = TextureManager::GetInstance()->Load("resources/sample/white.png");
+		}
+
+	}
+
+}
+
+void Particle3D::PreDraw(ID3D12GraphicsCommandList* commandList) {
+
+	assert(commandList_ == nullptr);
+
+	commandList_ = commandList;
+
+	worldTransformCamera_.UpdateMatrix();
+
+	//PSO設定
+	commandList_->SetPipelineState(particlePipelineStates_[currentBlendMode_]);
+	//ルートシグネチャを設定
+	commandList_->SetGraphicsRootSignature(rootSignature_);
+	//プリミティブ形状を設定
+	commandList_->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+}
+
+void Particle3D::PostDraw() {
+
+	commandList_ = nullptr;
+
+	modelNumber_ = 0;
+
+}
+
+void Particle3D::Draw(std::vector<WorldTransform> worldTransform) {
+
+	if (isBillboard_) {
+
+		matBillboard_ = Model::worldTransformCamera_.UpdateMatrix();
+
+		matBillboard_.m[3][0] = 0.0f;
+		matBillboard_.m[3][1] = 0.0f;
+		matBillboard_.m[3][2] = 0.0f;
+
+	}
+	else {
+		matBillboard_ = MakeIdentity4x4();
+	}
+
+	for (uint32_t i = 0; i < worldTransform.size(); i++) {
+
+		//配列外参照防止
+		if (i >= instanceCount_) {
+			break;
+		}
+		Matrix4x4 worldMatrix = MakeScaleMatrix(worldTransform[i].scale_) * matBillboard_ * MakeTranslateMatrix(worldTransform[i].translation_);
+		/*Matrix4x4 worldMatrix = worldTransform[i].matWorld_;*/
+		Matrix4x4 cameraMatrix = Model::worldTransformCamera_.UpdateMatrix();
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		matProjection_ = MakePerspectiveFovMatrix(0.45f, float(1280.0f) / float(720.0f), 0.1f, 1000.0f);
+		Matrix4x4 worldViewProjectionMatrix = worldMatrix * (viewMatrix * matProjection_);
+		matTransformMap_[i].WVP = worldViewProjectionMatrix;
+		matTransformMap_[i].World = worldMatrix;
+
+	}
+
+	//Spriteの描画
+	commandList_->IASetVertexBuffers(0, 1, &vbView_);
+	commandList_->SetGraphicsRootDescriptorTable(1, instancingResource_.srvHandleGPU);
+	////SRVの設定
+	commandList_->SetGraphicsRootDescriptorTable(2, texture_.srvHandleGPU);
+	commandList_->SetGraphicsRootConstantBufferView(0, constBuff_->GetGPUVirtualAddress());
+	//描画
+	commandList_->DrawInstanced(UINT(modelData_.vertices.size()), instanceCount_, 0, 0);
+
+}
+
+void Particle3D::Finalize() {
+
+	/*pipelineState_->Release();*/
+	for (int i = BlendMode::kCountBlend - 1; i >= 0; i--) {
+		particlePipelineStates_[i]->Release();
+	}
+	rootSignature_->Release();
+	ps3dParticleBlob_->Release();
+	vs3dParticleBlob_->Release();
+
+}
+
+void Particle3D::ImGuiUpdate() {
+
+	//識別ナンバー設定(ImGuiで使用)
+	std::string name = "model Color";
+
+	name += std::to_string(modelNumber_);
+
+	ImGui::Begin("model Settings");
+	ImGui::ColorEdit4(name.c_str(), &constMap_->color.x);
+	ImGui::End();
+
+	modelNumber_++;
+
+}
+
+//デバッグ時に使用
+void Particle3D::StaticImGuiUpdate() {
+
+	ImGui::Begin("Static particle Settings");
+	ImGui::Combo("Blend", (int*)&currentBlendMode_, BlendTexts, IM_ARRAYSIZE(BlendTexts));
+	ImGui::End();
+
+}
---
+++ Engine/3d/Particle3D.h
@@ -0,0 +1,154 @@
+#pragma once
+#include <wrl.h>
+#include <d3d12.h>
+#include <string>
+#include <vector>
+#include <dxcapi.h>
+#include "Engine/math/Matrix4x4.h"
+#include "Engine/math/Vector4.h"
+#include "Engine/math/Vector3.h"
+#include "Engine/math/Vector2.h"
+#include "WorldTransform.h"
+#include <vector>
+
+class Particle3D
+{
+public:
+
+	static void StaticInitialize(ID3D12Device* device);
+
+	struct VertexData {
+		Vector4 position;
+		Vector2 texcoord;
+		Vector3 normal;
+	};
+
+	struct Material
+	{
+		Vector4 color;
+		int32_t enableLighting;
+		float padding[3];
+		Matrix4x4 uvTransform;
+	};
+
+	struct DirectionalLight {
+		Vector4 color;
+		Vector3 direction;
+		float intensity;
+	};
+
+	struct MaterialData {
+		std::string textureFilePath;
+	};
+
+	struct ModelData {
+		std::vector<VertexData> vertices;
+		MaterialData material;
+	};
+
+	struct TransformationMatrix {
+		Matrix4x4 WVP;
+		Matrix4x4 World;
+	};
+
+	enum BlendMode {
+		kNormal,//通常
+		kAdd,//加算
+		kSubtract,//減算
+		kMultiply,//乗算
+		kScreen,//スクリーン
+
+		kCountBlend
+	};
+
+	static const char* BlendTexts[BlendMode::kCountBlend];
+
+	static Particle3D* Create(const std::string& filename, uint32_t instanceCount = 1);
+
+	void Initialize(const std::string& filename, uint32_t instanceCount);
+
+	static void PreDraw(ID3D12GraphicsCommandList* commandList);
+
+	static void PostDraw();
+
+	void Draw(std::vector<WorldTransform> worldTransform);
+
+	static void Finalize();
+
+	static WorldTransform worldTransformCamera_;
+
+	static 	Matrix4x4 matProjection_;
+
+	static void StaticImGuiUpdate();
+
+	void ImGuiUpdate();
+
+	static void SetBlendMode(BlendMode blendMode) { currentBlendMode_ = blendMode; }
+
+	//ビルボードを使うかどうか
+	bool isBillboard_ = true;
+
+private:
+
+	//静的メンバ変数
+
+	//デバイス
+	static ID3D12Device* device_;
+	//コマンドリスト
+	static ID3D12GraphicsCommandList* commandList_;
+	//ルートシグネチャ
+	static ID3D12RootSignature* rootSignature_;
+	//PSO
+	static ID3D12PipelineState* particlePipelineStates_[BlendMode::kCountBlend];
+
+	/*static ID3D12PipelineState* pipelineState_;*/
+
+	static IDxcBlob* vs3dParticleBlob_;
+	static IDxcBlob* ps3dParticleBlob_;
+
+	//モデル識別用変数(ImGuiで使用)
+	static int modelNumber_;
+
+	static BlendMode currentBlendMode_;
+
+private:
+
+	//頂点バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertBuff_;
+	Microsoft::WRL::ComPtr<ID3D12Resource> matBuff_;
+	//インデックスバッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexBuff_;
+	//定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuff_;
+	
+	//頂点バッファマップ
+	VertexData* vertMap_ = nullptr;
+	//インデックスバッファマップ
+	uint32_t* indexMap_ = nullptr;
+	//定数バッファマップ
+	Material* constMap_ = nullptr;
+	//TransformMatrix
+	TransformationMatrix* matTransformMap_ = nullptr;
+
+	//頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vbView_{};
+	//インデックスバッファビュー
+	D3D12_INDEX_BUFFER_VIEW ibView_{};
+
+	//ビルボード行列
+	Matrix4x4 matBillboard_;
+
+	//テクスチャ
+	Texture texture_;
+
+	//インスタンシングリソース
+	InstancingResource instancingResource_;
+
+	//モデルデータ
+	ModelData modelData_;
+
+	//インスタンシングの数
+	uint32_t instanceCount_;
+
+};
+
