f330e30f690df8dba3a0f4e6c0f8de71188f5408
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2024-02-14 01:05:33 +0900

    マージの不具合修正

--- Engines/Engine/base/WindowManager.cpp
+++ Engines/Engine/base/WindowManager.cpp
@@ -24,10 +24,6 @@ LRESULT WindowManager::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpa
 
 #endif // _DEBUG
 
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
-		return true;
-	}
-
 	//メッセージに応じてゲーム固有の処理を行う
 	switch (msg)
 	{
